bf2452304f3e7e6c7bb059e2ded2eacc72f4876e
iwasse <guilherme.iwasse@gmail.com>
2020-02-10 16:15:44 -0300

    otimização do código, secao 09, aula 61

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -26,13 +26,15 @@ const locators = {
         VALUE: '[data-test=valor]',
         INTERESTED: '[data-test=envolvido]',
         STATUS: '[data-test=status]',
+        ACC: '[data-test=conta]',
         SAVE_BTN: '.btn-primary'
     },
 
     EXTRATO: {
         LINHAS: '.list-group > li',
         FN_XP_SEARCH_ELEMENT: (desc, value) =>  `//span[contains(.,'${desc}')]/following-sibling::small[contains(., ${value})]`,
-        FN_XP_REMOVE_ELEMENT: acc => `//span[contains(.,'${acc}')]/../../..//i[@class='far fa-trash-alt']`
+        FN_XP_REMOVE_ELEMENT: acc => `//span[contains(.,'${acc}')]/../../..//i[@class='far fa-trash-alt']`,
+        FN_XP_EDIT_ELEMENT: acc1 => `//span[contains(.,'${acc1}')]/../../..//i[@class='fas fa-edit']`
     },
 
     SALDO: {
